1c092ccac0008c275b103b70a8238b1903016f12
hipiphock <hipiphock@naver.com>
2018-07-30 15:35:48 +0900

    finished finding

--- B+Tree - Disk-Based B+Tree/include/bpt.h
+++ B+Tree - Disk-Based B+Tree/include/bpt.h
@@ -1,8 +1,8 @@
-#ifndef _BPT_H_
-#define _BPT_H_
+#ifndef _BPT_H__
+#define _BPT_H__
 
 int open_db(const char* pathname);
-int insert(int64_t key, char* value);
+int insert(int64_t key, const char* value);
 char* find(int64_t key);
 int delete(int64_t key);
 
--- B+Tree - Disk-Based B+Tree/src/bpt.c
+++ B+Tree - Disk-Based B+Tree/src/bpt.c
@@ -1,168 +1,127 @@
 #include "bpt.h"
+#include "file.h"
 #include <stdio.h>
+#include <limits.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <stdint.h>
-#include <unistd.h>
+#include <string.h>
+#include <inttypes.h>
+#include <assert.h>
 #include <fcntl.h>
-#include <sys/types.h>
-
-#define PAGE_SIZE 4096
-#define RECORD_SIZE 128
-
-
-// BASIC STRUCTURES
-
-// first page, offset 0-4095, contains metadata
-typedef union _HeaderPage{
-    //free page offset
-    int64_t freePageOffset;
-    //root page offset
-    int64_t rootPageOffset;
-    //numbe of pages
-    int64_t numOfPages;
-    // empty space
-    char reserved[4072];
-} HeaderPage;
-
-// Free Page
-typedef union _FreePage{
-    //next page offset
-    int64_t nextFreePage;
-    // empty space 
-    char reserved[4088];
-} FreePage;
-
-// Page Header inside Leaf Page
-typedef union _PageHeader{
-    // parent page offset
-    int64_t parentPageOffset;
-    // is leaf?
-    int32_t isLeaf;
-    // number of keys
-    int32_t numOfKeys;
-    // empty space
-    char reserved[104];
-    // right sibling page offset
-    int64_t rightSiblingPageOffset;
-} PageHeader;
-
-// Record inside Leaf Page
-typedef union _Record{
-    int64_t key;
-    char value[120];
-} Record;
-
-// leaf page
-typedef union _LeafPage{
-    PageHeader pageHeader;
-    Record[31] record;
-} LeafPage;
-
-// key-offset is needed in Internal Page
-typedef union _KeyOffset{
-    int64_t key;
-    int64_t pageOffset;
-} KeyOffset;
-
-typedef union _InternalPage{
-    PageHeader pageHeader;
-    KeyOffset keyOffset[248];
-} InternalPage;
+#include <unistd.h>
 
 // GLOBAL VARIABLES
+extern HeaderPage headerPage;
+extern int fd;
 
-// file descriptor
-int fd;
 
-// header page
-HeaderPage* headerPage;
-
-// FUNCTIONS
-
-// reading file
-void* read_db(int64_t offset){
-    void* page = calloc(0, PAGE_SIZE);
-    lseek(fd, (off_t)offset, SEEK_SET);
-    if(read(fd, page, PAGE_SIZE) < 0){
-        free(page);
-        return NULL;
-    }
-    return page;
-}
-
-// writing file
-void write_db(void* page, int64_t offset){
-    lseek(fd, (off_t)offset, SEEK_SET);
-    write(fd, page, PAGE_SIZE);
-    fsync(fd);
-}
-
-// for opening DB
+// OPEN DB FILE
+// open a db file
 int open_db(const char* pathname){
     fd = open(pathname, O_RDWR);
     if(fd < 0){
-        fd = open(pathname, O_CREAT);
-        if(fd < 0)  return -1;
-        headerPage->freePageOffset = PAGE_SIZE;
-        headerPage->rootPageOffset = PAGE_SIZE * 2;
-        headerPage->numOfPages = 0;
-        write_db(headerPage, PAGE_SIZE);
-        return 0;
+        //create a new file
+        fd = open(pathname, O_CREAT|O_RDWR, S_IRUSR|S_IWUSR);
+        if(fd < 0){
+            //error : file is not created
+            return -1;
+        }
+    } else {
+        // file exist, so read it.
+        load_page(0, (Page*)&headerPage);
+        headerPage.file_offset = 0;
     }
     return 0;
 }
 
-// for finding
-
-// finding leaf
-LeafPage* find_leaf(int64_t key){
-    // first, fetch the root node
-    InternalPage* internalPage = (InternalPage)*read_db(headerPage->rootPageOffset);
-    while(!internalPage->pageHeader.isLeaf){
-        int left = 0, right = internalPage->pageHeader.numOfKeys - 1;
-        while(left < right){
-            int mid = (left + right) / 2;
-            int64_t mid_key = internalPage->keyOffset[mid].key;
-            if(key < mid_key) right = mid - 1;
-            else left = mid + 1
-        }
-        free(internalPage);
-        int64_t next_offset = internalPage->keyOffset[right].pageOffset;
-        internalPage = (InternalPage*)read_db(next_offset);
-    }
-    return (LeafPage*)internalPage;
+// Close a db file
+void close_db(){
+    close(fd);
 }
 
-// find function
-char* find(int64_t key){
-    LeafPage* leafPage = find_leaf(key);
-    int left = 0, right = leafPage->pageHeader.numOfKeys - 1;
-    while(left < right){
-        int mid = (left + right) / 2;
-        int64_t mid_key = leafPage->record[mid].key;
-        if(key < mid_key) right = mid - 1;
-        else if(key > mid_key) left = mid + 1;
+
+// FINDING
+// finds leaf
+// should handle binary search part
+LeafPage* find_leaf(int64_t key){
+    LeafPage* leafPage;
+    off_t root_offset = headerPage.root_offset;
+
+    if(root_offset == 0){
+        // there is no root
+        return NULL;
+    }
+
+    NodePage page;
+    load_page(root_offset, (Page*)&page);
+
+    while(!page.is_leaf){
+        InternalPage* internalPage = (InternalPage*)&page;
+        int64_t left = 0, right = INTERNAL_ORDER - 1, mid;
+        while(left <= right){
+            mid = (left + right) / 2;
+            if(key > InternalPage->internalRecords[mid].key){
+                left = mid + 1;
+            } else if (key < InternalPage->internalRecords[mid].key){
+                right = mid - 1;
+            } else {
+                load_page(InternalPage->internalRecords[mid].offset, (Page*)&page);
+                break;
+            }
+        }
+        if(key == InternalPage->internalRecords[mid].offset) continue;
         else{
-            free(leafPage);
-            return leafPage->record[mid].value;
+            // there is no matching key in file
+            return NULL;
         }
     }
-    free(leafPage);
+    return (LeafPage*)&page;
+}
+
+// returns value paired with key
+char* find(int64_t key){
+    char* ret_value;
+    LeafPage leafPage = find_leaf(key);
+    if(leafPage == NULL){
+        // no matching value
+        return NULL;
+    }
+    int64_t left = 0, right = LEAF_ORDER - 2, mid;
+    while(left <= right){
+        mid = (left + right) / 2;
+        if(key > leafPage->records[mid].key){
+            left = mid + 1;
+        } else is (key < leafPage->records[mid].key){
+            right = mid - 1;
+        } else {
+            ret_value = (char*)malloc(VALUE_SIZE * sizeof(char));
+            memcpy(ret_value, leafPage->records[mid].value);
+            return ret_value;
+        }
+    }
+    // no matching value - error
     return NULL;
 }
 
+
 // INSERTION
-Record make_record(int64_t key, char* value){
-    Record* record;
-    record = (Record*)malloc(sizeof(record));
-    // error handling needed
-    record->key = key;
-    Record->value = value;
-    return record;
+// insert into leaf
+void insert_into_leaf(){
+
 }
 
-int insert(int64_t key, char* value){
-    if(find(key) == NULL){
-        Record* newRecord = make_record(key, value);
-        // key에 근접한 자리를 찾자.
+// master function
+int insert(int64_t key, const char* value){
+
+    // case : the key already exist
+    if(find(key) != NULL){
+        // already exist!
+        return -1;
+    }
+
+    // case : inserting without splitting
+    if(find_leaf(key)->num_keys < LEAF_ORDER - 1){
+        
     }
 }

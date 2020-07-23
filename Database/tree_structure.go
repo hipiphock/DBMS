8b97b077859d399f6b76dd4ac3913aab4d1954ab
incorl_pc <hipiphock@naver.com>
2020-07-23 10:53:05 +0900

    go seems to be a little hard

---
+++ Database/tree_structure.go
@@ -0,0 +1,18 @@
+package main
+
+const RED = false
+const BLACK = true
+
+type TreeNode struct {
+	color  bool
+	key    int8
+	record string
+}
+
+func init_node() TreeNode {
+	return TreeNode(false, 0, 0)
+}
+
+func insert(parent_node TreeNode, insert_node TreeNode) {
+
+}
